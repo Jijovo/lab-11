28f524e22740e570f947a1549723fc6bb36f6dd8
Jijovo <jijovo77@gmail.com>
2026-02-22 12:30:17 -0800

    update 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 // Ezzat Mohamadein | CompSC 210 | Lab 11
 using namespace std;
 #include <iostream>
+#include <fstream>
+
+//creates a football (soccer) club struct.
+//contains the club name, rank in leaderboard, and a dynamic array with the players' names.
+struct Club {
+    string clubName;
+    int rank;
+    string* players[11];
+};
 
 int main() {
+    //creates a dynamic array of 3 football clubs.
+    Club* clubs = new Club[3];
+    //initializes the club names, ranks, and players' names for each club.
+    clubs[0].clubName = "Real Madrid";
     
     return 0;
 }
