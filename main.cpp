6340213a48ab9074c3471931a74daaee25b8b2a6
Jijovo <jijovo77@gmail.com>
2026-02-22 12:46:53 -0800

    final

--- main.cpp
+++ main.cpp
@@ -16,34 +16,44 @@ int main() {
     Club* clubs = new Club[3];
     //initializes the club names, ranks, and players' names for each club.
     ifstream fin ("input.txt");
-    clubs[0].clubName = "Real Madrid"; //club 1 (guess who I support).
-    clubs[0].rank = 1;
-    clubs[0].players = new string[11];
-    for (int i = 0; i < 11; i++) {
-        getline(fin, clubs[0].players[i]);
-    }
-    clubs[1].clubName = "Barcelona"; //club 2
-    clubs[1].rank = 2;
-    clubs[1].players = new string[11];
-    for (int i = 0; i < 11; i++) {
-        getline(fin, clubs[1].players[i]);
-    }
-    clubs[2].clubName = "Atletico Madrid"; //club 3
-    clubs[2].rank = 3;
-    clubs[2].players = new string[11];
-    for (int i = 0; i < 11; i++) {
-        getline(fin, clubs[2].players[i]);
-    }
-    fin.close();
-    //prints the club names, ranks, and players' names for each club.
-    for (int i = 0; i < 3; i++) {
-        cout << "Club Name: " << clubs[i].clubName << endl;
-        cout << "Rank: " << clubs[i].rank << endl;
-        cout << "Players: " << endl;
-        for (int j = 0; j < 11; j++) {
-            cout << clubs[i].players[j] << " ";
+    if (fin.good()) {
+        clubs[0].clubName = "Real Madrid"; //club 1 (guess who I support).
+        clubs[0].rank = 1;
+        clubs[0].players = new string[11];
+        for (int i = 0; i < 11; i++) {
+            getline(fin, clubs[0].players[i]);
         }
-        cout << endl;
+        clubs[1].clubName = "Barcelona"; //club 2
+        clubs[1].rank = 2;
+        clubs[1].players = new string[11];
+        for (int i = 0; i < 11; i++) {
+            getline(fin, clubs[1].players[i]);
+        }
+        clubs[2].clubName = "Atletico Madrid"; //club 3
+        clubs[2].rank = 3;
+        clubs[2].players = new string[11];
+        for (int i = 0; i < 11; i++) {
+            getline(fin, clubs[2].players[i]);
+        }
+        fin.close();
+        //prints the club names, ranks, and players' names for each club.
+        for (int i = 0; i < 3; i++) {
+            cout << "Club Name: " << clubs[i].clubName << endl;
+            cout << "Rank: " << clubs[i].rank << endl;
+            cout << "Players: " << endl;
+            for (int j = 0; j < 11; j++) {
+                cout << clubs[i].players[j] << " ";
+            }
+            cout << endl << endl;
+        }
+        //deletes the dynamic array of clubs and the dynamic arrays of players for each club to free up memory.
+        for (int i = 0; i < 3; i++) {
+            delete[] clubs[i].players;
+        }
+        delete[] clubs;
+    }
+    else {
+        cout << "Error opening file." << endl;
     }
     return 0;
 }
